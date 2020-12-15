333a6126730f460c482cb2b410a43331ae444ed5
Ashley616Z <1210782824@qq.com>
2020-12-15 10:13:49 +0800

    第二十三次课

---
+++ 第二十二次课/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+function orange(){
+    console.log("this is orange 1")
+    setTimeout(function(){
+        console.log("this is orange 2")
+        setTimeout(function(){
+            console.log("this is orange2-1")
+        },3000)
+    },2000)
+    console.log("this is orange 3")
+}
+
+orange()
+
+
+
+
